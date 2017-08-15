01e906a0d86b8af59afafd1b7cb0fd67390ba0ed
Piotr Czapowski <czapowski.piotr92@gmail.com>
2017-08-15 22:10:01 +0200

    First commit
    Added two basic aplications: shop and cart.

---
+++ cart/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class CartAddProductForm(forms.Form):
+    quantity = forms.IntegerField(max_value=10, min_value=1, initial=1)
+    update  = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
---
+++ shop/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category,Product
+from cart.forms import CartAddProductForm
+
+def product_list(request, category_slug=None):
+
+    category=None
+    categories =Category.objects.all()
+    products=Product.objects.filter(available=True)
+    # for c in categories:
+    #     products += Product.objects.filter(available=True,category=c )[:3]
+    # cart_product_form=CartAddProductForm(initial={'quantity': 1, 'update': False })
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category, available=True)
+    return render(request, 'shop/product/list.html', {'category' : category,
+                                                      'categories': categories,
+                                                      'products' : products
+                                                      })
+
+def product_detail(request, id, slug):
+    categories=Category.objects.all()
+    product= get_object_or_404(Product, id= id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/product/detail.html', {'product':product,
+                                                        'categories':categories,
+                                                        'cart_product_form': cart_product_form
+                                                        })
+
